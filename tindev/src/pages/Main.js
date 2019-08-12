21b54e32e79a89a298427d33940d1fd803741469
Victor Georg Oliveira <vgeorgoliveira@gmail.com>
2019-08-11 22:40:49 -0300

    Match feature on mobile completed

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import AsyncStorage from '@react-native-community/async-storage';
 import { View, Text, SafeAreaView, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import api from '../services/api';
@@ -6,11 +7,13 @@ import api from '../services/api';
 import logo from '../assets/logo.png';
 import dislike from '../assets/dislike.png';
 import like from '../assets/like.png';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ navigation })
 {
     const _id = navigation.getParam('user');
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadDevs() {
@@ -22,6 +25,16 @@ export default function Main({ navigation })
         }
 
         loadDevs();
+    }, [_id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: _id },
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        });
     }, [_id]);
 
     async function handleLike()
@@ -94,6 +107,20 @@ export default function Main({ navigation })
                     </View>
                 )
             }
+
+            { matchDev && (
+                <View style={styles.matchContainer}>
+                    <Image style={styles.matchImage} source={itsamatch} />
+                    <Image style={styles.matchAvatar} source={{ uri: matchDev.avatar }} />
+
+                    <Text style={styles.matchName}>{matchDev.name}</Text>
+                    <Text style={styles.matchBio}>{matchDev.bio}</Text>
+
+                    <TouchableOpacity onPress={() => setMatchDev(null)}>
+                        <Text style={styles.closeMatch}>Close</Text>
+                    </TouchableOpacity>
+                </View>
+            ) }
             
         </SafeAreaView>
     )
@@ -184,4 +211,48 @@ const styles = StyleSheet.create({
             height: 2,
         },
     },
+
+    matchContainer: {
+        ... StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    matchImage: {
+        height: 60,
+        resizeMode: 'contain'
+    },
+
+    matchAvatar: {
+        width: 160,
+        height: 160,
+        borderRadius: 80,
+        borderWidth: 5,
+        borderColor: '#fff',
+        marginVertical: 30,
+    },
+
+    matchName: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
+
+    matchBio: {
+        marginTop: 10,
+        fontSize: 16,
+        color: 'rgba(255,255,255, 0.8)',
+        lineHeight: 24,
+        textAlign: 'center',
+        paddingHorizontal: 30,
+    },
+
+    closeMatch: {
+        fontSize: 16,
+        color: 'rgba(255,255,255, 0.8)',
+        textAlign: 'center',
+        marginTop: 30,
+        fontWeight: 'bold',
+    }
 });
